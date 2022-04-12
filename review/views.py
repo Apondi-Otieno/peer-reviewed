e4ec4f0d3c8036c880b5dd247844f981ba652a95
Apondi-Otieno <apondiotii@gmail.com>
2022-04-12 21:35:55 +0300

    this will add imports to views file

--- review/views.py
+++ review/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect, get_object_or_404
+from .forms import NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from .models import Follow, Post, Profile, Comment, Like
+from .forms import ProfileUpdateForm, UserUpdateForm, NewPostForm, CommentForm
+from django.http import HttpResponseRedirect
 # Create your views here.
+
+
+def home(request):
+    
