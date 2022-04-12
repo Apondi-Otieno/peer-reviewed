71ff29f509114f546bc0b0c2b54776f7991b3fca
Apondi-Otieno <apondiotii@gmail.com>
2022-04-12 22:02:58 +0300

    this will add post class

--- review/models.py
+++ review/models.py
@@ -21,3 +21,28 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+
+class Post(models.Model):
+    image = CloudinaryField('image')
+    title = models.CharField(max_length=30, default='')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default='', null=True ,related_name='author')
+    likes = models.IntegerField(default=0)
+    caption = models.TextField(max_length=300)
+
+    @classmethod
+    def all_posts(cls) :
+        posts = cls.objects.all()
+        return posts
+
+    def save_post(self):
+        self.save()
+
+    def delete_post(self):
+        self.delete()
+
+    @classmethod
+    def update_post(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+
+
