7d2ff25bd53f96b61ea151fc0953f4ac9a6d09d3
Apondi-Otieno <apondiotii@gmail.com>
2022-04-12 21:31:01 +0300

    this will add procfile, urls.py file

--- review/urls.py
+++ review/urls.py
@@ -3,3 +3,8 @@ from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 
+urlpatterns = [
+    path("home", views.home, name="home"),
+]
+
+"add attr home to views.py file"
